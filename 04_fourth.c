0f162e5796a94d9bffdfc16638ecd2a843b49a92
Abhitoshworks <abhitoshyt@gmail.com>
2026-01-20 21:00:18 +0530

    Day 4: Fix syntax errors and comments in fourth.c
    Fixed newline character syntax and added missing semicolon. Corrected comment formatting to use standard C style.

---
+++ 04_fourth.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    int a; //it has no role in it just wanted to comment
+    char ch = 'a';
+    ch = 'c';
+    ch = '\l';
+    printf("hello i love aloo \l fries too");
+    return 0;
+
+
+}
