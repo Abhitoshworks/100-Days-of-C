32b92824ba35ef809a70e8e9f60694d1fbbed3d5
Abhitoshworks <abhitoshyt@gmail.com>
2026-01-20 21:08:16 +0530

    Update 04_fourth.c

--- 04_fourth.c
+++ 04_fourth.c
@@ -3,9 +3,10 @@ int main (){
     int a; //it has no role in it just wanted to comment
     char ch = 'a';
     ch = 'c';
-    ch = '\l';
-    printf("hello i love aloo \l fries too");
+    ch = '\n';
+    printf("hello i love aloo \n fries too");
     return 0;
+
 
 
 }
