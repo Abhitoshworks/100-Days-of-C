0fc0de2b654ffe83f9f107566500483332820e4e
Abhitoshworks <abhitoshyt@gmail.com>
2026-01-22 19:49:10 +0530

    Q6 P1 done volume of cylinder + area of circle

---
+++ 06_problem2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int r = 5;
+    int h = 2;
+    printf("the area of circle with radius %d is %f",r,3.14*r*r);
+    printf("the volume of cylinder with radius %d height %d is %f" ,r,h,3.14*r*r*h);
+    return 0;
+
+
+
+}
