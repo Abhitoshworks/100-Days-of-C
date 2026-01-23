ecd63eb760910e60d3a832eb92f6edd8b235f592
Abhitoshworks <abhitoshyt@gmail.com>
2026-01-23 19:41:24 +0530

    (Centigrade degrees temperature to Fahrenheit).

--- 06_problem2.c
+++ 06_problem2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
-    int r = 5;
-    int h = 2;
-    printf("the area of circle with radius %d is %f",r,3.14*r*r);
-    printf("the volume of cylinder with radius %d height %d is %f" ,r,h,3.14*r*r*h);
+    float c = 35.0,f;
+    f = (9.0/5.0)*c + 32;
+    printf("Centigrade degrees temperature to Fahrenheit is %f",f);
     return 0;
+
 
 
 
