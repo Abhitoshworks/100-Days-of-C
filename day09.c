7d7d64ec1d4f68d0581b957d35dee8b14f98ce05
Abhitoshworks <abhitoshyt@gmail.com>
2026-01-25 19:12:19 +0530

    day 09/ Arithmetic operations

---
+++ day09.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a = 6;
+    int b = 5;
+    int c = a+b;
+    printf("the value of a is %d and the value of b is %d and sum is %d\n",a,b,c);
+    printf("the remainder when a is divided by b is %d",a%b);
+    return 0;
+
+
+
+}
