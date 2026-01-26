7c292bed4fa4df3d6cc7a372cc53071c482f6989
Abhitoshworks <abhitoshyt@gmail.com>
2026-01-26 21:19:05 +0530

    arithmetic operation of int/float

---
+++ day10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a = 5;
+    int b = 2;
+    float c = 5/2; //output should be 2.5 but it wont cuz arithmetic of int is int 
+    printf("The value of a/b is %f",c);
+    return 0;
+
+
+}
