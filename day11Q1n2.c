43cc4d1222f211e63036164df1c85ea0b6ab0462
Abhitoshworks <abhitoshyt@gmail.com>
2026-01-27 20:17:00 +0530

    day11/theoryQuestions

---
+++ day11Q1n2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    //Q1 Which of the following is invalid in C?
+    int a=1; int b = a;
+    int v = 3*3;
+    char dt = ‘21 dec 2020’; //this one cuz char can only hold one character
+    return 0;
+}
+   //Q2 What data type will 3.0/8 – 2 return?
+   //answer - float
